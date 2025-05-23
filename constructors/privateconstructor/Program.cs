dc241bf26e24ca997186a3e4dafa128614af4f49
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-05-23 16:36:54 +0545

    chore!:private constructors added

---
+++ constructors/privateconstructor/Program.cs
@@ -0,0 +1,25 @@
+﻿class Student
+{
+    private int roll;
+    private Student()
+    {
+        roll = 10;
+        Console.WriteLine("private constructor called");
+    }
+    public static Student CreateInstance()
+    {
+        return new Student();
+    }
+    public void display()
+    {
+        Console.WriteLine(roll);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Student s = Student.CreateInstance();
+        s.display();
+    }
+}
