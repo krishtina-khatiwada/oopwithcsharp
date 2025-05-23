8215fb8e4439ef544b60499f10afd66e96fb006c
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-05-23 10:55:30 +0545

    chore:single inheritance added

---
+++ inheritance/singleinheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿class Student
+{
+    public void record()
+    {
+        string name;
+        int roll;
+        name = "ram";
+        roll = 10;
+        Console.WriteLine($"{name} {roll}");
+    }
+}
+class Course : Student
+{
+    public void subject()
+    {
+        Console.WriteLine("Maths");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Course c = new Course();
+        c.subject();
+        c.record();
+    }
+}
