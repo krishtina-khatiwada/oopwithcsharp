e4b6068fcb07d508ac9ac0b44f7caaeab2c87ccb
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-05-23 12:36:40 +0545

    chore:added file abstract class

---
+++ abstraction/abstractclass/Program.cs
@@ -0,0 +1,20 @@
+﻿abstract class Student
+{
+    public abstract void Record();
+}
+class Subject : Student
+{
+    public override void Record()
+    {
+        string subname = "C sharp";
+        Console.WriteLine("sub name is " + subname);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Subject s1 = new Subject();
+        s1.Record();
+    }
+}
