fcc59d897094959fa11155ba7df16b01fcbf845d
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-05-23 15:04:35 +0545

    chore: added file constructors

---
+++ constructors/defaultconstructor/Program.cs
@@ -0,0 +1,17 @@
+﻿class Student
+{
+    public Student()
+    {
+        string name = "ram";
+        int age = 10;
+        Console.WriteLine($"{name} {age}");
+    }
+
+}
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+    }
+}
