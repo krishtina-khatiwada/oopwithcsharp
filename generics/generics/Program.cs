ef203d292a182fb9fd93259fe0000eaed560758f
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-05-26 11:19:15 +0545

    chore:new branch generics added

---
+++ generics/generics/Program.cs
@@ -0,0 +1,16 @@
+﻿class Cat<T>
+{
+    public void Meow<T>(T item)
+    {
+        Console.WriteLine("the cat has a toy" + item);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Cat<int> c = new Cat<int>();
+        c.Meow(12);
+
+    }
+}
