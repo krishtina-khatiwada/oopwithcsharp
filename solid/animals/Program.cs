c014ed5351924d8f64672edc0192e1374849c843
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-05-27 13:15:28 +0545

    chore: Interface segregation principle
    new folder added

---
+++ solid/animals/Program.cs
@@ -0,0 +1,53 @@
+﻿interface ISwim
+{
+    void Icanswim();
+}
+interface IWalk
+{
+    void Icanwalk();
+}
+interface IFly
+{
+    void Icanfly();
+}
+class Dog : IWalk, ISwim
+{
+    public void Icanwalk()
+    {
+        Console.WriteLine("Dog can walk");
+    }
+    public void Icanswim()
+    {
+        Console.WriteLine("Dog can swim");
+    }
+}
+class Parrot :IFly
+{
+    public void Icanfly()
+    {
+        Console.WriteLine("Parrot can fly");
+    }
+    
+}
+class Fish : ISwim
+{
+    public void Icanswim()
+    {
+        Console.WriteLine("fish can walk");
+    }
+    
+}
+class Program
+{
+    static void Main()
+    {
+        Dog d = new();
+        Parrot p = new Parrot();
+        Fish f = new Fish();
+        d.Icanswim();
+        d.Icanwalk();
+        p.Icanfly();
+        f.Icanswim();
+        
+    }
+}
