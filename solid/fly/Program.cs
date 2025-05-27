d6ba0a31ab44e2026f02cb3427c22e48f12c84c0
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-05-27 12:46:49 +0545

    Implementation of LSV

---
+++ solid/fly/Program.cs
@@ -0,0 +1,33 @@
+﻿interface Ifly
+{
+    void Ifly();
+}
+
+class Bird
+{
+
+}
+class Parrot : Bird, Ifly
+{
+    public void Ifly()
+    {
+        Console.WriteLine("This bird can fly");
+    }
+}
+class Penguin:Bird
+{
+    public void Pen()
+    {
+        Console.WriteLine("penguin cannot fly");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Parrot p = new Parrot();
+        p.Ifly();
+        Penguin Pe = new Penguin();
+        Pe.Pen();
+    }
+}
